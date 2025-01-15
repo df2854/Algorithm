a386436f3b87a942b9366811c0c75b6dcb06c4e4
이창환 <106215825+df2854@users.noreply.github.com>
2025-01-15 23:15:15 +0900

    [Bronze II] Title: 공 바꾸기, Time: 124 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n+1];
+		for (int i = 1; i <= n; i++) {
+			arr[i] = i;
+		}
+		
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			int tmp;
+			
+			tmp = arr[a];
+			arr[a] = arr[b];
+			arr[b] = tmp;
+		}
+		
+		for (int i = 1; i <= n; i++) {
+			bw.write(arr[i] + " ");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
