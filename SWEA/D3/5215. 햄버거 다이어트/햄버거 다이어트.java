4b72c53e7bad0786b967a2d8a86d592ae07eb570
이창환 <106215825+df2854@users.noreply.github.com>
2025-02-20 15:40:17 +0900

    [D3] Title: 햄버거 다이어트, Time: 138 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,51 +1,42 @@
-import java.io.*;
 import java.util.*;
- 
-class Solution
-{
-    static int N, L;
-    static int[][] food;
-    static int result;
-     
-    public static void main(String args[]) throws Exception
-    {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-         
-        int test = Integer.parseInt(br.readLine());
-        for (int t = 1; t < test + 1; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            L = Integer.parseInt(st.nextToken());
-             
-            food = new int[N][2];
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                food[i][0] = Integer.parseInt(st.nextToken());
-                food[i][1] = Integer.parseInt(st.nextToken());
-            }
-            result = 0;
-             
-            bt(0, 0, 0);
-             
-            bw.write("#" + t + " " + result + "\n");
-        }
-        bw.flush();
-        bw.close();
-    }
-     
-    public static void bt(int n, int score, int cal) {
-        if (n == N) {
-            result = Math.max(score, result);
-            return;
-        }
-         
-        if (cal + food[n][1] <= L) {
-            bt(n + 1, score + food[n][0], cal + food[n][1]);
-        }
-         
-        if (cal <= L) {
-            bt(n + 1, score, cal);
-        }
-    }
+import java.io.*;
+
+class Solution {
+	static int[][] ham;
+	static int n, l, result;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int tc = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= tc; t++) {
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			l = Integer.parseInt(st.nextToken());
+			
+			ham = new int[n][2];
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				ham[i][0] = Integer.parseInt(st.nextToken());
+				ham[i][1] = Integer.parseInt(st.nextToken());
+			}
+			
+			result = 0;
+			dfs(0, 0, 0);
+			
+			System.out.println("#" + t + " " + result);
+		}
+	}
+	private static void dfs(int cnt, int score, int cal) {
+		if (cnt == n) {
+			result = Math.max(result, score);
+			return;
+		}
+		
+		if (cal + ham[cnt][1] <= l) {
+			dfs (cnt + 1, score + ham[cnt][0], cal + ham[cnt][1]);
+		} 
+		if (cal <= l) {
+			dfs (cnt + 1, score, cal);
+		}
+	}
 }
