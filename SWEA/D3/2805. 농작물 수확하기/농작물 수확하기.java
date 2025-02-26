c6da0acef42e9bcffb76fc71f09e4bc88f6b27b0
이창환 <106215825+df2854@users.noreply.github.com>
2025-02-26 16:28:43 +0900

    [D3] Title: 농작물 수확하기, Time: 120 ms, Memory: 31,616 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,36 +1,44 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int test = Integer.parseInt(br.readLine());
-        for (int t = 1; t < test + 1; t++) {
-            int n = Integer.parseInt(br.readLine());
-            int[][] map = new int[n][n];
-
-            String[] input = new String[n];
-            for (int i = 0; i < n; i++) {
-                input = br.readLine().split("");
-                for (int j = 0; j < n; j++) {
-                    map[i][j] = Integer.parseInt(input[j]);
-                }
-            }
-
-            int h = n / 2;
-            int value = 0;
-            for (int i = 0; i < n; i++) {
-                if (h >= i) {
-                    for (int j = h - i; j < h + i + 1; j++) {
-                        value += map[i][j];
-                    }
-                } else {
-                    for (int j = i - h; j < n - (i - h); j++) {
-                        value += map[i][j];
-                    }
-                }
-            }
-            System.out.println("#" + t + " " + value);
-        }
+	static int n, map[][];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] input;
+		
+		int tc = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= tc; t++) {
+			n = Integer.parseInt(br.readLine());
+			
+			map = new int[n][n];
+			input = new String[n];
+			for (int i = 0; i < n; i++) {
+				input = br.readLine().split("");
+				for (int j = 0; j < n; j++) {
+					map[i][j] = Integer.parseInt(input[j]);
+				}
+			}
+			
+			int h = n / 2;
+			int result = 0;
+			
+			for (int i = 0; i < n; i++) {
+				if (i <= h) {
+					for (int j = h - i; j < h + i + 1; j++) {
+						result += map[i][j];
+					}
+				} else {
+					for (int j = i - h; j < n - (i - h); j++) {
+						result += map[i][j];
+					}
+				}
+			}
+			
+			bw.write("#" + t + " " + result + "\n");
+		}
+		bw.flush();
+		bw.close();
 	}
 }
