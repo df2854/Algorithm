3abccc729b9a30e85c880371afc895a46e963fe5
이창환 <106215825+df2854@users.noreply.github.com>
2025-03-04 16:25:38 +0900

    [Silver II] Title: 예산, Time: 116 ms, Memory: 13552 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		
+		int end = Integer.MIN_VALUE;
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			end = Math.max(end, arr[i]);
+		}
+		int limit = Integer.parseInt(br.readLine());
+		
+		int start = 0;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			int sum = 0;
+			for (int i = 0; i < n; i++) {
+				if (mid >= arr[i]) {
+					sum += arr[i];
+				} else {
+					sum += mid;
+				}
+			}
+			
+			if (sum <= limit) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+		
+		bw.write(end + "\n");
+		bw.flush();
+		bw.close();
+	}
+}
