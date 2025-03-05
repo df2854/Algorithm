09302dab8d319dd8d492a62d4ee6bcf3279e60cf
이창환 <106215825+df2854@users.noreply.github.com>
2025-03-05 14:11:14 +0900

    [Gold V] Title: 배열 돌리기 1, Time: 508 ms, Memory: 38304 KB -BaekjoonHub

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -2,9 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int n, m, r, arr[][], count;
-	public static void main(String[] args) throws IOException {
+	static int n, m, r;
+	static int[][] arr;
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
@@ -18,36 +22,44 @@ public class Main {
 			}
 		}
 		
-		count = Math.min(n, m) / 2;
+		int cnt = Math.min(n, m) / 2; // 돌려야할 배열 수 
+		
 		for (int i = 0; i < r; i++) {
-			for (int j = 0; j < count; j++) {
+			for (int j = 0; j < cnt; j++) {
 				int tmp = arr[j][j];
 				
+				// 위쪽
 				for (int k = j + 1; k < m - j; k++) {
 					arr[j][k-1] = arr[j][k];
 				}
 				
+				// 오른쪽
 				for (int k = j + 1; k < n - j; k++) {
-					arr[k-1][m-1-j] = arr[k][m-1-j];
+					arr[k-1][m - j - 1] = arr[k][m - j - 1];
 				}
 				
-				for (int k = m - 2 - j; k >= j; k--) {
-					arr[n-1-j][k+1] = arr[n-1-j][k];
+				// 아래쪽
+				for (int k = m - j - 2; k >= j; k--) {
+					arr[n - j - 1][k+1] = arr[n - j - 1][k];
 				}
 				
-				for (int k = n - 2 - j; k >= j + 1; k--) {
+				// 왼쪽
+				for (int k = n - j - 2; k >= j + 1 ; k--) {
 					arr[k+1][j] = arr[k][j];
 				}
 				
-				arr[j+1][j] = tmp;
+				arr[j + 1][j] = tmp;
 			}
 		}
 		
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				System.out.print(arr[i][j] + " ");
+				sb.append(arr[i][j]).append(" ");
 			}
-			System.out.println();
+			sb.append("\n");
 		}
+		bw.write(sb + "\n");
+		bw.flush();
+		bw.close();
 	}
 }
