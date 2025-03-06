abf8a1430ce2fb1b4e9fbfcce6a8a37e6cb83250
이창환 <106215825+df2854@users.noreply.github.com>
2025-03-06 14:46:51 +0900

    [Silver II] Title: 촌수계산, Time: 72 ms, Memory: 11708 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n, m, x, y, cnt, result;
+	static int[] dist;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		n = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		x = Integer.parseInt(st.nextToken());
+		y = Integer.parseInt(st.nextToken());
+		
+		List<Integer>[] list = new ArrayList[n+1];
+		for (int i = 0; i <= n; i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		m = Integer.parseInt(br.readLine());
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			list[a].add(b);
+			list[b].add(a);
+		}
+		
+		dist = new int[n+1];
+		Arrays.fill(dist, -1);
+		
+		Queue<Integer> que = new ArrayDeque<>();
+		que.offer(x);
+		dist[x] = 0;
+		while(!que.isEmpty()) {
+			int current = que.poll();
+			for (int num : list[current]) {
+				if (dist[num] == -1) {
+					dist[num] = dist[current] + 1;
+					que.offer(num);
+				}
+			}
+		}
+		System.out.println(dist[y]);
+	}
+}
