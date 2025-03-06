f277ff33d5e7bd3d2aa46eb07142b42daa1e309d
이창환 <106215825+df2854@users.noreply.github.com>
2025-03-06 13:52:57 +0900

    [Silver I] Title: 미로 탐색, Time: 128 ms, Memory: 13600 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,51 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, -1};
-    static int n, m;
-    static int[][] graph;
-
-    static class Point {
-        public int y, x;
-        Point(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        graph = new int[n][m];
-        String[] input;
-        for(int i = 0; i < n; i++) {
-            input = br.readLine().split("");
-            for (int j = 0; j < m; j++) {
-                graph[i][j] = Integer.parseInt(input[j]);
-            }
-        }
-
-        Queue<Point> que = new LinkedList<>();
-        graph[0][0] = -1;
-        que.offer(new Point(0, 0));
-        while (!que.isEmpty()) {
-            Point cur = que.poll();
-            int cy = cur.y, cx = cur.x;
-
-            for(int dir = 0; dir < 4; dir++) {
-                int ny = cy + dy[dir], nx = cx + dx[dir];
-
-                if (0 <= ny && ny < n && 0 <= nx && nx < m && graph[ny][nx] == 1) {
-                    graph[ny][nx] = graph[cy][cx] - 1;
-                    que.offer(new Point(ny, nx));
-                }
-            }
-        }
-
-        System.out.println(-graph[n-1][m-1]);
-    }
+	static int n, m;
+	static int[][] map;
+	static int[][] d = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		map = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			String[] input = br.readLine().split("");
+			for (int j = 0; j < m; j++) {
+				map[i][j] = Integer.parseInt(input[j]);
+			}
+		}
+		
+		Queue<int[]> que = new ArrayDeque<>();
+		que.offer(new int[] {0, 0});
+		while(!que.isEmpty()) {
+			int[] cur = que.poll();
+			
+			if (cur[0] == n - 1 && cur[1] == m - 1) break;
+			
+			for (int dir = 0; dir < 4; dir++) {
+				int ny = cur[0] + d[dir][0];
+				int nx = cur[1] + d[dir][1];
+				
+				if (ny >= 0 && ny < n && nx >= 0 && nx < m && map[ny][nx] == 1) {
+					map[ny][nx] += map[cur[0]][cur[1]];
+					que.offer(new int[] {ny, nx});
+				}
+			}
+		}
+		
+		System.out.println(map[n-1][m-1]);
+	}
 }
