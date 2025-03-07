04a8e648356cc7cbff0ed3cab006092b513386d0
이창환 <106215825+df2854@users.noreply.github.com>
2025-03-07 16:12:06 +0900

    [Gold II] Title: 빵집, Time: 320 ms, Memory: 41960 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -2,51 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int r, c, result;
-	static char[][] map;
-	static boolean flag;
-	static boolean[][] visited;
+	static int r, c, cnt;
 	static int[][] dir = {{-1, 1}, {0, 1}, {1, 1}};
-	public static void main(String[] args) throws IOException {
+	static char[][] map;
+	static boolean[][] visited;
+	static boolean flag;
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		r = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());
-		
 		map = new char[r][c];
 		visited = new boolean[r][c];
+		
 		for (int i = 0; i < r; i++) {
-			String str = br.readLine();
+			String input = br.readLine();
 			for (int j = 0; j < c; j++) {
-				map[i][j] = str.charAt(j);
+				map[i][j] = input.charAt(j);
 			}
 		}
 		
-		result = 0;
+		cnt = 0;
 		
 		for (int i = 0; i < r; i++) {
 			flag = false;
-			dfs(i, 0);
+			dfs(i ,0);
 		}
 		
-		System.out.println(result);
+		System.out.println(cnt);
 	}
-	private static void dfs(int row, int col) {
-		if (col == c-1) {
-			result++;
+	private static void dfs(int y, int x) {
+		if (x == c - 1) {
+			cnt++;
+			visited[y][x] = true;
 			flag = true;
-			visited[row][col] = true;
 			return;
 		}
 		
-		for (int d = 0; d < 3; d++) {
-			int nr = row + dir[d][0];
-			int nc = col + dir[d][1];
+		for (int[] d : dir) {
+			int ny = y + d[0];
+			int nx = x + d[1];
 			
-			if(nr >= 0 && nr < r && nc >= 0 && nc < c && map[nr][nc] == '.' && !visited[nr][nc]) {
-				dfs(nr, nc);
-				visited[row][col] = true;
+			if (ny >= 0 && ny < r && nx >= 0 && nx < c && map[ny][nx] == '.' && !visited[ny][nx]) {
+				visited[ny][nx] = true;
+				dfs(ny, nx);
 				if (flag) return;
 			}
 		}
