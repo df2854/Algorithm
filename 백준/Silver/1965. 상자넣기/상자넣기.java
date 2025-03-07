2678caa0254bdceaf1b53f1726575d8e64283e08
이창환 <106215825+df2854@users.noreply.github.com>
2025-03-07 13:35:49 +0900

    [Silver II] Title: 상자넣기, Time: 72 ms, Memory: 11916 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.java
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -2,31 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int n, result;
-	static int[] arr, dp;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		n = Integer.parseInt(br.readLine());
-		
-		arr = new int[n];
-		dp = new int[n];
-		result = Integer.MIN_VALUE;
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			dp[i] = 1;
-			
-			for (int j = 0; j < i; j++) {
-				if (arr[i] > arr[j]) {
-					dp[i] = Math.max(dp[i], dp[j] + 1);
-				}
-			}
-			
-			result = Math.max(result, dp[i]);
-		}
-		
-		System.out.println(result);
-	}
+    static int n;
+    static int[] arr;
+    static ArrayList<Integer> lis;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        lis = new ArrayList<>();
+        
+        for (int num : arr) {
+            int pos = Collections.binarySearch(lis, num);
+            if (pos < 0) {
+                pos = -(pos + 1);
+            }
+            if (pos == lis.size()) {
+                lis.add(num);
+            } else {
+                lis.set(pos, num);
+            }
+        }
+        
+        System.out.println(lis.size());
+    }
 }
