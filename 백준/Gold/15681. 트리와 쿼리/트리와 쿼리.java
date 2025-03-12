ab3e1a55ea656c20f35e665ed220d41468fac34e
이창환 <106215825+df2854@users.noreply.github.com>
2025-03-12 16:51:40 +0900

    [Gold V] Title: 트리와 쿼리, Time: 1652 ms, Memory: 100268 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -2,7 +2,7 @@
  접근 방법 : 그래프 탐색, 처음에 루트에서 U까지 방문 처리(dfs)하고 다시 루트 U인 서브트리 정점의 수를 계산(dfs)
  dfs를 두 번 하니까 시간초과  (N, Q <= 10^5이므로 당연히 시간 초과)
  그래서 트리의 각 노드의 depth를 저장 ->  서브 트리 루트의 depth보다 큰 노드만 dfs -> 그런데 이번에는 메모리 초과
- visited말고 다른 방법 필요 dp?
+ visited말고 다른 방법 필요 -> subtree 배열에 각 노드의 서브 트리 노드의 수 dfs로 저장
  */
 
 import java.io.*;
@@ -10,7 +10,7 @@ import java.util.*;
 
 public class Main {
 	static int n, r, q;
-	static int[] dp;
+	static int[] subtree;
 	static List<Integer>[] list;
 	static boolean[] visited;
 	public static void main(String[] args) throws Exception {
@@ -35,21 +35,21 @@ public class Main {
 			list[end].add(start);
 		}
 
-		dp = new int[n+1];
-		Arrays.fill(dp, 1);
+		subtree = new int[n+1];
+		Arrays.fill(subtree, 1); // 자기 자신은 포함
 		
 		dfs(r, 0);
 		
 		for (int i = 0; i < q; i++) {
 			int root = Integer.parseInt(br.readLine());
-			System.out.println(dp[root]);
+			System.out.println(subtree[root]);
 		}
 	}
-	private static void dfs(int node, int parent) {
+	private static void dfs(int node, int parent) {// 현재 노드와 부모 노드로 dfs
 		for (int i : list[node]) {
 			if (i != parent) {
 				dfs(i, node);
-				dp[node] += dp[i];
+				subtree[node] += subtree[i];
 			}
 		}
 	}
