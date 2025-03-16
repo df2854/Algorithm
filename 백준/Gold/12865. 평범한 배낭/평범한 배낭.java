af8059cd5bf853f47d4d2efa4dc439dc7e5f167e
이창환 <106215825+df2854@users.noreply.github.com>
2025-03-17 01:19:58 +0900

    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 51348 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n, k;
+	static int[][] arr;
+	static int[][] dp;
+ 	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		
+		arr = new int[n][2];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		dp = new int[n+1][k+1];
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= k; j++) {
+				if (j < arr[i - 1][0]) {
+					dp[i][j] = dp[i-1][j];
+				} else {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i - 1][j - arr[i-1][0]] + arr[i-1][1]);
+				}
+			}
+		}
+		
+		System.out.println(dp[n][k]);
+	}
+}
