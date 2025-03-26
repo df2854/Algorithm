473619ab5ea9de8c6bcbe62dd12cc3641589b97b
이창환 <106215825+df2854@users.noreply.github.com>
2025-03-26 14:51:50 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 106 ms, Memory: 28,544 KB -BaekjoonHub

---
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+	static class UnionFind {
+		int[] parent;
+		int[] rank;
+		
+		UnionFind(int size) {
+			parent = new int[size+1];
+			rank = new int[size+1];
+			
+			for (int i = 1; i <= size; i++) {
+				parent[i] = i;
+				rank[i] = 0;
+			}
+		}
+		
+		int find(int x) {
+			if (parent[x] != x) {
+				parent[x] = find(parent[x]);
+			}
+			
+			return parent[x];
+		}
+		
+		void union(int x, int y) {
+			int rootX = find(x);
+			int rootY = find(y);
+			
+			if (rootX == rootY) return;
+			
+			if (rank[rootX] < rank[rootY]) {
+				parent[rootX] = rootY;
+			} else if (rank[rootY] > rank[rootX]) {
+				parent[rootY] = rootX;
+			} else {
+				parent[rootY] = rootX;
+				rank[rootX]++;
+			}
+		}
+	}
+	static int tc, n, m;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		tc = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= tc; t++) {
+			StringBuilder sb = new StringBuilder();
+			sb.append("#" + t + " ");
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			
+			UnionFind u = new UnionFind(n);
+			for (int i = 0; i < m; i++) {
+				st = new StringTokenizer(br.readLine());
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				u.union(x, y);
+			}
+			
+			Set<Integer> set = new HashSet<>();
+			for (int i = 1; i <= n; i++) {
+				set.add(u.find(i));
+			}
+			sb.append(set.size());
+			
+			bw.write(sb + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
