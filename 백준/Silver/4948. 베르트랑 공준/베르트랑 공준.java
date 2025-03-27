05bf85429f9d65746f5f5f4a4e72888b8351bbdf
이창환 <106215825+df2854@users.noreply.github.com>
2025-03-27 10:47:15 +0900

    [Silver II] Title: 베르트랑 공준, Time: 156 ms, Memory: 24144 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -15,24 +15,29 @@ public class Main {
 			n = Integer.parseInt(br.readLine());
 			if (n == 0) break;
 			
-			isPrime = new boolean[2*n + 1];
-			Arrays.fill(isPrime, true);
-			isPrime[0] = false;
-			isPrime[1] = false;
+			prime(n);
 			
-			for (int i = 2; i <= Math.sqrt(2*n); i++) {
-				if(isPrime[i]) {
-					for (int j = i * i; j <= 2*n; j += i) {
-						isPrime[j] = false;
-					}
-				}
-			}
 			int count = 0;
 			for (int i = n+1; i <= 2 * n; i++) {
 				if (isPrime[i]) count++;
 			}
 			
 			System.out.println(count);
+		}
+	}
+	
+	private static void prime(int num) {
+		isPrime = new boolean[2*num + 1];
+		Arrays.fill(isPrime, true);
+		isPrime[0] = false;
+		isPrime[1] = false;
+		
+		for (int i = 2; i <= Math.sqrt(2*num); i++) {
+			if(isPrime[i]) {
+				for (int j = i * i; j <= 2*num; j += i) {
+					isPrime[j] = false;
+				}
+			}
 		}
 	}
 }
