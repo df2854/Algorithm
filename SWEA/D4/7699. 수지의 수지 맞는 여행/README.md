5a05286e6bd123a9d7f3b4b19d1523073af70237
이창환 <106215825+df2854@users.noreply.github.com>
2025-03-28 00:39:51 +0900

    [D4] Title: 수지의 수지 맞는 여행, Time: 6,080 ms, Memory: 28,084 KB -BaekjoonHub

---
+++ SWEA/D4/7699. 수지의 수지 맞는 여행/README.md
@@ -0,0 +1,15 @@
+# [D4] 수지의 수지 맞는 여행 - 7699 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWqUzj0arpkDFARG) 
+
+### 성능 요약
+
+메모리: 28,084 KB, 시간: 6,080 ms, 코드길이: 1,306 Bytes
+
+### 제출 일자
+
+2025-03-28 00:39
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/7699. 수지의 수지 맞는 여행/수지의 수지 맞는 여행.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+	static int tc, r, c, result;
+	static char[][] map;
+	static List<Character> list;
+	static int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		tc = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= tc; t++) {
+			st = new StringTokenizer(br.readLine());
+			r = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
+			
+			map = new char[r][c];
+			for (int i = 0; i < r; i++) {
+				String input = br.readLine();
+				for (int j = 0; j < c; j++) {
+					map[i][j] = input.charAt(j);
+				}
+			}
+			
+			list = new ArrayList<>();
+			list.add(map[0][0]);
+			
+			result = -1;
+			
+			dfs(0, 0, 1);
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("#" + t + " " + result);
+			System.out.println(sb);
+		}
+	}
+	private static void dfs(int i, int j, int depth) {
+		result = Math.max(result, depth);
+		for (int[] d : dir) {
+			int ny = i + d[0];
+			int nx = j + d[1];
+			
+			if (ny < 0 || nx < 0 || ny >= r || nx >= c) continue;
+			if (list.contains(map[ny][nx])) continue;
+			
+			list.add(map[ny][nx]);
+			dfs(ny, nx, depth+1);
+			list.remove(list.size() - 1);
+		}
+	}
+}
