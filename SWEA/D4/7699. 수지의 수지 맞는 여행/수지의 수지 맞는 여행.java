c2d0bec4c5c30404701067d06fe95dee1c6049ec
이창환 <106215825+df2854@users.noreply.github.com>
2025-03-28 00:53:09 +0900

    [D4] Title: 수지의 수지 맞는 여행, Time: 1,228 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D4/7699. 수지의 수지 맞는 여행/수지의 수지 맞는 여행.java
+++ SWEA/D4/7699. 수지의 수지 맞는 여행/수지의 수지 맞는 여행.java
@@ -4,7 +4,6 @@ import java.util.*;
 class Solution {
 	static int tc, r, c, result;
 	static char[][] map;
-	static List<Character> list;
 	static int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,29 +24,25 @@ class Solution {
 				}
 			}
 			
-			list = new ArrayList<>();
-			list.add(map[0][0]);
-			
 			result = -1;
 			
-			dfs(0, 0, 1);
+			dfs(0, 0, 1, 1 << (map[0][0] - 'A'));
 			
 			sb.append("#").append(t).append(" ").append(result).append("\n");
 		}
 		System.out.println(sb);
 	}
-	private static void dfs(int i, int j, int depth) {
+	private static void dfs(int i, int j, int depth, int bit) {
 		result = Math.max(result, depth);
 		for (int[] d : dir) {
 			int ny = i + d[0];
 			int nx = j + d[1];
 			
 			if (ny < 0 || nx < 0 || ny >= r || nx >= c) continue;
-			if (list.contains(map[ny][nx])) continue;
-			
-			list.add(map[ny][nx]);
-			dfs(ny, nx, depth+1);
-			list.remove(list.size() - 1);
+			int c = map[ny][nx] - 'A';
+			if ((bit | (1 << c)) == bit) continue;
+		
+			dfs(ny, nx, depth+1, bit | (1 << c));
 		}
 	}
 }
