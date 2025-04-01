cd437bffcd0dddc27f78609b5f1f116e61e8350d
이창환 <106215825+df2854@users.noreply.github.com>
2025-04-01 11:53:19 +0900

    [Gold IV] Title: 최단경로, Time: 584 ms, Memory: 116256 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,75 +1,80 @@
 import java.io.*;
 import java.util.*;
 
-class Node {
-    int v;
-    int w;
-
-    Node(int v, int w) {
-        this.v = v;
-        this.w = w;
-    }
-    
-}
-
 public class Main {
-    private static void dijkstra (List<List<Node>> graph, int start) {
-        int n = graph.size();
-        int[] dis = new int[n];
-        Arrays.fill(dis, Integer.MAX_VALUE);
+	static class Vertex implements Comparable<Vertex> {
+		int no, weight;
 
-        dis[start] = 0;
-        PriorityQueue<Node> que = new PriorityQueue<>((o1, o2) -> {
-        	return o1.w - o2.w;
-        });
-        que.offer(new Node(start, 0));
+		public Vertex(int no, int weight) {
+			super();
+			this.no = no;
+			this.weight = weight;
+		}
 
-        while (!que.isEmpty()) {
-            Node cur = que.poll();
-            int curV = cur.v;
-            int curW = cur.w;
-
-            if (curW > dis[curV]) continue;
-
-            for (Node node : graph.get(curV)) {
-                int newDis = dis[curV] + node.w;
-
-                if (newDis < dis[node.v]) {
-                    dis[node.v] = newDis;
-                    que.offer(new Node(node.v, newDis));
-                }
-            }
-        }
-
-        for (int i = 1; i <= n-1; i++) {
-            System.out.println((dis[i] == Integer.MAX_VALUE) ? "INF" : dis[i]);
-        }
-    }
-    public static void main(String[] args) throws IOException {
+		@Override
+		public int compareTo(Vertex o) {
+			return Integer.compare(this.weight, o.weight);
+		}
+	}
+	static int v, e, k, result;
+	static int[] dist;
+	static boolean[] visited;
+	static List<Vertex>[] vertexList;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-
-        List<List<Node>> graph = new ArrayList<>();
-
-        for (int i = 0; i <= V; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        int start = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-
-            graph.get(u).add(new Node(v, w));
-        }
-        
-
-        dijkstra(graph, start);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		v = Integer.parseInt(st.nextToken());
+		e = Integer.parseInt(st.nextToken());
+		
+		k = Integer.parseInt(br.readLine());
+		
+		vertexList = new ArrayList[v+1];
+		for (int i = 0; i <= v; i++) {
+			vertexList[i] = new ArrayList<>();
+		}
+		for (int i = 0; i < e; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			
+			vertexList[u].add(new Vertex(v, w));
+		}
+		
+		dist = new int[v+1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		visited = new boolean[v+1];
+		
+		PriorityQueue<Vertex> pq = new PriorityQueue<>();
+		dist[k] = 0;
+		pq.offer(new Vertex(k, 0));
+			
+		while(!pq.isEmpty()) {
+			Vertex cur = pq.poll();
+			
+			int curV = cur.no;
+			int curW = cur.weight;
+			
+			if (visited[curV]) continue;
+			
+			visited[curV] = true;
+			
+			for (Vertex n : vertexList[curV]) {
+				int newDis = dist[curV] + n.weight;
+				
+				if (dist[n.no] > newDis) {
+					dist[n.no] = newDis;
+					pq.offer(new Vertex(n.no, dist[n.no]));
+				}
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= v; i++) {
+			if (dist[i] == Integer.MAX_VALUE) sb.append("INF").append("\n");
+			else sb.append(dist[i]).append("\n");
+		}
+		
+		System.out.println(sb);
 	}
 }
