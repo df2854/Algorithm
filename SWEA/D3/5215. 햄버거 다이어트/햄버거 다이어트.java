bd451811d3ce913e1a11a8afd2915b9e9f9392ab
이창환 <106215825+df2854@users.noreply.github.com>
2025-04-04 15:29:40 +0900

    [D3] Title: 햄버거 다이어트, Time: 143 ms, Memory: 38,560 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,42 +1,42 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Solution {
-	static int[][] ham;
-	static int n, l, result;
-	public static void main(String[] args) throws IOException {
+	static int tc, n, l;
+	static int[][] arr, dp;
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		int tc = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		tc = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= tc; t++) {
 			st = new StringTokenizer(br.readLine());
 			n = Integer.parseInt(st.nextToken());
 			l = Integer.parseInt(st.nextToken());
 			
-			ham = new int[n][2];
+			arr = new int[n][2];
+			
 			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
-				ham[i][0] = Integer.parseInt(st.nextToken());
-				ham[i][1] = Integer.parseInt(st.nextToken());
+				for (int j = 0; j < 2; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
 			}
 			
-			result = 0;
-			dfs(0, 0, 0);
+			dp = new int[n+1][l+1];
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= l; j++) {
+					if (j < arr[i-1][1]) dp[i][j] = dp[i-1][j];
+					else {
+						dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - arr[i-1][1]] + arr[i-1][0]);
+					}
+				}
+			}
 			
-			System.out.println("#" + t + " " + result);
+			sb.append("#").append(t).append(" ").append(dp[n][l]).append("\n");
 		}
+		System.out.println(sb);
 	}
-	private static void dfs(int cnt, int score, int cal) {
-		if (cnt == n) {
-			result = Math.max(result, score);
-			return;
-		}
-		
-		if (cal + ham[cnt][1] <= l) {
-			dfs (cnt + 1, score + ham[cnt][0], cal + ham[cnt][1]);
-		} 
-		if (cal <= l) {
-			dfs (cnt + 1, score, cal);
-		}
-	}
+	
 }
