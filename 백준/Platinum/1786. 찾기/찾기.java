20c300cbc4042b514283b33a86e8b5dfb9000f73
이창환 <106215825+df2854@users.noreply.github.com>
2025-04-10 16:18:09 +0900

    [Platinum V] Title: 찾기, Time: 2404 ms, Memory: 130128 KB -BaekjoonHub

---
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] text = br.readLine().toCharArray();
+		char[] pattern = br.readLine().toCharArray();
+		int tLength = text.length;
+		int pLength = pattern.length;
+		
+		int[] pi = new int[pLength]; // 부분 일치 테이블 (실패 함수) 만들기 : 패턴에서 불일치가 발생할 경우 활용해서 패턴 포인터 이동 목적
+		for (int i = 1, j = 0; i < pLength; i++) { // 패턴에서 패턴을 찾는 원리를 이용
+			// i : 패턴의 접미사, j : 패턴의 접두사
+			// 두 포인터의 위치에서 불일치가 발생하면 맞은 직전 위치의 정보를 활용해서 불필요한 비교를 줄임
+			while(j > 0 && pattern[i] != pattern[j]) {
+				j = pi[j-1];
+			}
+			
+			// 현재 i위치까지의 부분 문자열의 접미사가 접두사와 일치하는 패턴의 최장 길이 저장
+			if (pattern[i] == pattern[j]) {
+				pi[i] = ++j; // j위치까지 일치한 경우는 j+1, 후에 j 뒤로 이동
+			} else {
+				pi[i] = 0; // 일치하는 접두사 접미사 없음
+			}
+		}
+		
+		int cnt = 0;
+		List<Integer> list = new ArrayList<>();
+		
+		for (int i = 0, j = 0; i < tLength; i++) {
+			while (j > 0 && text[i] != pattern[j]) {
+				j = pi[j-1];
+			}
+			
+			if (text[i] == pattern[j]) {
+				if (j == pLength-1) { // 일치가 발생한 위치가 패턴의 끝이면
+					++cnt; // 패턴 찾았으므로 카운트 증가
+					list.add(i-j);
+					j = pi[j];
+				} else {
+					++j;
+				}
+			}
+		}
+		
+		System.out.println(cnt);
+		if (cnt > 0) {
+			for (int i = 0; i < cnt; i++) {
+				System.out.print(list.get(i) + 1 + " ");
+			}
+		}
+	}
+} 
