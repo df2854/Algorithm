4c53d4c7bfa401822fc690b4450e001d93028311
이창환 <106215825+df2854@users.noreply.github.com>
2025-08-20 14:42:24 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 504 ms, Memory: 68092 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,12 +1,16 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+	
 	static class Vertex implements Comparable<Vertex> {
-		int no, weight;
-		
-		Vertex (int no, int weight) {
-			this.no = no;
+		int node, weight;
+
+		public Vertex(int node, int weight) {
+			super();
+			this.node = node;
 			this.weight = weight;
 		}
 
@@ -15,32 +19,36 @@ public class Main {
 			return Integer.compare(this.weight, o.weight);
 		}
 	}
-	static int n, e, v1, v2;
-	static long result;
-	static List<Vertex>[] edgeList;
-	public static void main(String[] args) throws IOException {
+	
+	private static int N, E, v1, v2;
+	private static long result;
+	private static List<Vertex>[] adjList;
+	
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
 		
-		n = Integer.parseInt(st.nextToken());
-		e = Integer.parseInt(st.nextToken());
+		adjList = new ArrayList[N+1];
 		
-		edgeList = new ArrayList[n+1];
-		for (int i = 0; i <= n; i++) {
-			edgeList[i] = new ArrayList<>();
+		for (int i = 0; i <= N; i++) {
+			adjList[i] = new ArrayList<>();
 		}
 		
-		for (int i = 0; i < e; i++) {
+		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			
-			edgeList[a].add(new Vertex(b, c));
-			edgeList[b].add(new Vertex(a, c));
+			adjList[a].add(new Vertex(b, c));
+			adjList[b].add(new Vertex(a, c));
 		}
 		
 		st = new StringTokenizer(br.readLine());
+		
 		v1 = Integer.parseInt(st.nextToken());
 		v2 = Integer.parseInt(st.nextToken());
 		
@@ -48,34 +56,35 @@ public class Main {
 		int[] fromV1 = dijkstra(v1);
 		int[] fromV2 = dijkstra(v2);
 		
-		long path1 = (long) from1[v1] + fromV1[v2] + fromV2[n];
-		long path2 = (long) from1[v2] + fromV2[v1] + fromV1[n];
+		long a = (long) from1[v1] + fromV1[v2] + fromV2[N];
+		long b = (long) from1[v2] + fromV2[v1] + fromV1[N];
 		
-		result = Math.min(path1, path2);
+		result = Math.min(a, b);
 		
 		System.out.println(result >= Integer.MAX_VALUE ? -1 : result);
 	}
-	private static int[] dijkstra(int start) {
-		PriorityQueue<Vertex> pq = new PriorityQueue<>();
-		int[] dist = new int[n+1];
+	
+	public static int[] dijkstra(int start) {
+		int[] dist = new int[N+1];
 		Arrays.fill(dist, Integer.MAX_VALUE);
-		
 		dist[start] = 0;
-		pq.offer(new Vertex(start, dist[start]));
 		
-		while (!pq.isEmpty()) {
-			Vertex cur = pq.poll();
+		PriorityQueue<Vertex> pq = new PriorityQueue<>();
+		pq.offer(new Vertex(start, 0));
+		
+		while(!pq.isEmpty()) {
+			Vertex no = pq.poll();
 			
-			for (Vertex next : edgeList[cur.no]) {
-				int newDist = dist[cur.no] + next.weight;
-				
-				if (dist[next.no] > newDist) {
-					dist[next.no] = newDist;
-					pq.offer(new Vertex(next.no, dist[next.no]));
+			if (no.weight > dist[no.node]) continue;
+			
+			for (Vertex next : adjList[no.node]) {
+				if (dist[next.node] > next.weight + dist[no.node]) {
+					dist[next.node] = next.weight + dist[no.node];
+					pq.offer(new Vertex(next.node, dist[next.node]));
 				}
 			}
 		}
 		
 		return dist;
-	}	
+	}
 }
